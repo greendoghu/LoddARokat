19e9d067beea66c41c92019f1a412ed8e31b231e
greendoghu <greendoghu@gmail.com>
2013-06-06 18:51:57 +0200

    1st

--- LoddARokat/gen/co/hu/greendog/loddarokat/R.java
+++ LoddARokat/gen/co/hu/greendog/loddarokat/R.java
@@ -21,24 +21,21 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int cube=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int button1=0x7f080004;
-        public static final int editText1=0x7f080003;
-        public static final int imageView1=0x7f080000;
-        public static final int linearLayout1=0x7f080001;
-        public static final int textView1=0x7f080002;
-        public static final int textView2=0x7f080005;
-        public static final int textView3=0x7f080006;
+        public static final int action_settings=0x7f080005;
+        public static final int adView=0x7f080004;
+        public static final int button1=0x7f080001;
+        public static final int button2=0x7f080002;
+        public static final int button3=0x7f080003;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int menu_layout=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int menu=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
